8ad41762ac2646d5d5802467c6e7db182accb8cd
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-07-07 13:00:15 +0200

    changed card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
-
-import './card-list.styles.css';
+import "./card-list.styles.css";
 import Card from "../card/card.component";
-class CardList extends Component {
-  render() {
-    console.log('render');
-    console.log(this.props);
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster}></Card>;
-        })}
-      </div>
-    );
-  }
-}
+
+const CardList = ({monsters}) => {
+
+  // instead of {monsters} can be done also as below
+  // const { monsters } = props;
+
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster}></Card>;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
